e822d7c9705f7ee5ab6f9d98b91e787d55d1dbb1
vani_projects <tshravani@gmail.com>
2020-08-20 23:21:40 -0600

    question7

---
+++ VogCodeChallenge.API/Data/EmployeeDBRepository.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using VogCodeChallenge.API.Models;
+
+namespace VogCodeChallenge.API.Data
+{
+    public class EmployeeDBRepository:IEmployeeRepository
+    {
+        public EmployeeDBRepository()
+        {
+            //DB Connection Initialization
+        }
+
+        public void Delete(int employeeId)
+        {
+            // Implement DB delete
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Employee> GetAll()
+        {
+            // Implement DB select all
+            throw new NotImplementedException();
+        }
+
+        public Employee GetById(int employeeId)
+        {
+            // Implement DB select
+            throw new NotImplementedException();
+        }
+
+        public void Insert(Employee employee)
+        {
+            // Implement DB Insert
+            throw new NotImplementedException();
+        }
+
+        public void Save()
+        {
+            // Implement DB Update
+            throw new NotImplementedException();
+        }
+
+        public void Update(Employee employee)
+        {
+            // Implement DB Update
+            throw new NotImplementedException();
+        }
+    }
+}
