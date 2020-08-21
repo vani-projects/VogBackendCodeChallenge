d368c442781e56de96c81f440ec6024a6b9e31e3
Shravani <tshravani@gmail.com>
2020-08-20 21:39:41 -0600

    question2

---
+++ VogCodeChallenge.API/Models/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace VogCodeChallenge.API.Models
+{
+    public class Employee
+    {
+
+        public int Id { get; set; }
+        [Display(Name = "Department")]
+        public int DepartmentId { get; set; }
+        [Display(Name = "First Name")]
+        public string FisrtName { get; set; }
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        [Display(Name = "Job Title")]
+        public string JobTitle { get; set; }
+        [Display(Name = "Mailing address")]
+        public string MailingAddress { get; set; }
+
+    }
+}
