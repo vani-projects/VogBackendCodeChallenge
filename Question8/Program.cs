52e9f012cb23c7edc257cfed45cfa6be75ffabca
vani_projects <tshravani@gmail.com>
2020-08-20 23:27:08 -0600

    question8

---
+++ Question8/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Question8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            QuestionClass.TestQuestion();
+        }
+    }
+}
---
+++ Question8/QuestionClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Question8
+{
+    public static class QuestionClass
+    {
+        static List<string> NamesList = new List<string>()
+        {
+            "Jimmy",
+            "jeffrey"
+        };
+
+        public static void TestQuestion()
+        {
+            NamesList.ForEach(x => Console.WriteLine(x));
+        }
+    }
+
+}
